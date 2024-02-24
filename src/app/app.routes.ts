1f956efe3aef428967daa5042e769622fc537e66
DavZala <zalasardavib@gmail.com>
2024-02-23 19:52:13 -0500

    actualizacion 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './home/home.component';
 import { DetailVersion1Component } from './detail-version1/detail-version1.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent},
+   { path: '', component: HomeComponent},
     { path: 'login', component: LoginComponent },
     { path: 'detailsdisney', component: DetailVersion1Component},
     { path: '**', component: PageNotFoundComponent }
